dac24c12e0c0ae9f12915cfb06627d1eb74d763b
gihun2225 <gihun2225@gmail.com>
2022-12-20 11:48:58 +0900

    점수 기능 구현

--- source.c
+++ source.c
@@ -4,37 +4,51 @@
 
 int main()
 {
+	int score = 0;
 	srand(time(NULL));
 	int num1 = rand() % 10;
 	int num2 = rand() % 10;
 	int ch = rand() % 4;
-	printf("%d ", num1);
-	switch (ch)
-	{
-	case 0:
-		printf("+");
-	case 1:
-		printf("-");
-	case 2:
-		printf("*");
-	case 3:
-		printf("/");
-	}
-	printf(" %d", num2);
-	int ans;
-	scanf("%d", ans);
 	int res;
-	switch (ch)
+	int ans;
+	while (1)
 	{
-	case 0:
-		res = num1 + num2;
-	case 1:
-		res = num1 - num2;
-	case 2:
-		res = num1 * num2;
-	case 3:
-		res = num1 / num2;
+		printf("%d ", num1);
+		switch (ch)
+		{
+		case 0:
+			printf("+");
+		case 1:
+			printf("-");
+		case 2:
+			printf("*");
+		case 3:
+			printf("/");
+		}
+		printf(" %d", num2);
+		scanf("%d", ans);
+
+		switch (ch)
+		{
+		case 0:
+			res = num1 + num2;
+		case 1:
+			res = num1 - num2;
+		case 2:
+			res = num1 * num2;
+		case 3:
+			res = num1 / num2;
+		}
+		if (ans == res)
+		{
+			score++;
+			printf("정답입니다. 점수: %d", score);
+		}
+		else
+		{
+			printf("오답입니다. 점수: %d", score);
+		}
+
 	}
-	if (ans == res)
-		printf("정답입니다.");
+
 }
